dd8cb0cc03b09ba9a7d9dc3c984037ad9ed069ba
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-03-18 13:00:17 +0100

    somma 2

--- somma.py
+++ somma.py
@@ -1,5 +1,5 @@
-valore1=input("pass primo valore:")
-valore2=input("pass seocndo valore:")
+valore1=input("pass primo valore: ")
+valore2=input("pass seocndo valore: ")
 v1= int(valore1)
 v2= int(valore2)
 somma= v1+v2    
