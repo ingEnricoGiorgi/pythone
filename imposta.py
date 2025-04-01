2bc8537639e71ab26ab816d25c1a9a303da4d2fc
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-04-01 13:07:35 +0200

    autoformat

--- imposta.py
+++ imposta.py
@@ -1,12 +1,12 @@
-#Shift + Alt + F
+# Shift + Alt + F
 reddito = float(input("Immetti il tuo reddito in numeri: \n"))
 scaglione1 = 28000
-scaglione2 = 50000      
+scaglione2 = 50000
 residuo = 22000
 
 if reddito <= 0:
     print("Il reddito deve essere maggiore di 0.")
-else: 
+else:
     if reddito <= scaglione1:
         tasse = reddito * 0.23
         print("Paghi il 23% di tasse: ", tasse)
@@ -15,5 +15,10 @@ else:
             tasse = (scaglione1 * 0.23) + ((reddito - scaglione1) * 0.35)
             print("Paghi il 23% fino a 28.000 €, e il 35% sul resto: ", tasse)
         else:
-            tasse = (scaglione1 * 0.23) + (residuo * 0.35) + ((reddito - scaglione2) * 0.43)
-            print("Paghi il 23% fino a 28.000 €, il 35% fino a 50.000 €, e il 43% sul resto: ", tasse)
+            tasse = (
+                (scaglione1 * 0.23) + (residuo * 0.35) + ((reddito - scaglione2) * 0.43)
+            )
+            print(
+                "Paghi il 23% fino a 28.000 €, il 35% fino a 50.000 €, e il 43% sul resto: ",
+                tasse,
+            )
