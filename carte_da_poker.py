fb0fcb4a5ef7eb40a2de617973a0d20f5e0f22be
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-04-01 10:15:53 +0200

    adw

--- carte_da_poker.py
+++
@@ -1,9 +0,0 @@
-cuori =b"\xE2\x99\xA1"
-quadri =b"\xE2\x99\xA2"
-picche =b"\xE2\x99\xA3"
-fiori =b"\xE2\x99\xA4"
-stringaCuori = cuori.decode()
-stringaQuadri = quadri.decode()
-stringaPicche = picche.decode()
-stringaFiori = fiori.decode()
-print(stringaCuori+" "+stringaQuadri+" "+stringaPicche+" "+stringaFiori)
