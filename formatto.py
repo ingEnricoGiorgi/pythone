d8f0fcb554a87b119c5b643a560149475574294e
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-04-09 11:43:14 +0200

    formatto

--- formatto.py
+++ formatto.py
@@ -1,7 +1,9 @@
 #formatto.py
 
 reciprociList=[]
-number = [1,2,3,4,5,6,7,8,9,10]
-for i in range(len(number)):
-    reciprociList.append(round(float(1 / number[i]),8))
+number = 11
+for i in range(number):
+    #reciprociList.append(round(float(1 / number[i]),8))
+    reciproco = 1 / (i+1)
+    reciprociList.append(f"{reciproco:10.8f}")
 print(reciprociList)
