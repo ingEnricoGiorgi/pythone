5d12baaef5ffc4455d055333236c80cb24f81483
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-04-15 12:12:51 +0200

    underscore

--- lattina.py
+++ lattina.py
@@ -3,19 +3,19 @@ import math
 class SodaCan:
     #costruttore
     def __init__(self, height, radius):
-        self.height = height
-        self.radius = radius
+        self._height = height
+        self._radius = radius
 
     #self perche è metodo
     def getSurfaceArea(self):
         # Surface Area = 2 * π * r * h + 2 * π * r^2
-        lateral_area = 2 * math.pi * self.radius * self.height
-        top_bottom_area = 2 * math.pi * self.radius ** 2
+        lateral_area = 2 * math.pi * self._radius * self._height
+        top_bottom_area = 2 * math.pi * self._radius ** 2
         return lateral_area + top_bottom_area
 
     def getVolume(self):
         # Volume = π * r^2 * h
-        return math.pi * self.radius ** 2 * self.height
+        return math.pi * self._radius ** 2 * self._height
     
 soda = SodaCan(height=10, radius=2)
 
