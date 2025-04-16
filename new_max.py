68918b74fc20a54f1a419fced5be527470b7d635
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2025-04-16 11:01:03 +0200

    prova

--- new_max.py
+++ new_max.py
@@ -10,6 +10,10 @@ def main():
     media = sum(numbers) / len(numbers)
     print(f"La media è: {media}")
     print(f"Il valore massimo trovato nel file è: {max_value}")
+    print(f"valore trovato alla posizione: {numbers.index(max_value)+1}")
 
-if __name__ == "__main__":
-    main()
+#ci garantisce che viene eseguito solo se il file viene eseguito direttamente
+#In pratica, mettendo la funzione main() sotto questa condizione, ti assicuri 
+# che venga eseguita solo quando avvii direttamente lo script, ma non quando qualcun altro importa il tuo file come modulo.
+#if __name__ == "__main__":
+main()
